f41e5bf34fc960085e8f496e43aa656cbc3b1345
Ruaridh Mollica <ruaridhmollica@gmail.com>
2020-12-27 15:13:09 +0000

    now serving index.html using gin

--- main.go
+++ main.go
@@ -1,37 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(GetPort(), nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	p := "." + r.URL.Path
-	if p == "./" {
-		p = "./templates/index.html"
-	}
-	http.ServeFile(w, r, p)
-}
+	port := os.Getenv("PORT")
 
-// Get the Port from the environment so we can run on Heroku
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	// Set a default port if there is nothing in the environment
 	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No PORT detected, defaulting to " + port)
+		log.Fatal("$PORT must be set")
 	}
-	return ":" + port
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.html")
+	router.Static("/public", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	router.Run(":" + port)
 }
