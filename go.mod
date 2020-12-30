1add34e6e1910d9f90ae339311e42097fbdf4e3e
Ruaridh Mollica <ruaridhmollica@gmail.com>
2020-12-30 11:44:16 +0000

    database connection test

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jonahgeorge/force-ssl-heroku v1.0.1
+	github.com/lib/pq v1.9.0
 )
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	//"database/sql"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	heroku "github.com/jonahgeorge/force-ssl-heroku"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -17,10 +20,10 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	//db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	//if err != nil {
-	//	log.Fatalf("Error opening database: %q", err)
-	//}
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -32,6 +35,38 @@ func main() {
 	})
 	router.GET("/tour", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "tour.html", nil)
+	})
+
+	router.GET("/tick", func(c *gin.Context) {
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error creating database table: %q", err))
+			return
+		}
+
+		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error incrementing tick: %q", err))
+			return
+		}
+
+		rows, err := db.Query("SELECT tick FROM ticks")
+		if err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error reading ticks: %q", err))
+			return
+		}
+
+		defer rows.Close()
+		for rows.Next() {
+			var tick time.Time
+			if err := rows.Scan(&tick); err != nil {
+				c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error scanning ticks: %q", err))
+				return
+			}
+			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+		}
 	})
 
 	router.Run(":" + port)
