80472517aa027f2646a54d292ec8cf9057e2404a
Ruaridh Mollica <ruaridhmollica@gmail.com>
2020-12-30 17:52:55 +0000

    Fixes and Updates

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,24 @@ exec 10<&0
     echo "Building Go files..."
     eval "go build -o bin/trail -v ." 
     if [ $? -eq 0 ] 
-     #Notifies the user that a the openssl command has evaluated with the key $LINE$i
      then echo "Build Complete" 
     fi
     echo "Tidying...."
     eval "go mod tidy" 
     if [ $? -eq 0 ] 
-     #Notifies the user that a the openssl command has evaluated with the key $LINE$i
      then echo "Tidied." 
     fi
     echo "Updating dependencies...."
     eval "go mod vendor" 
     if [ $? -eq 0 ] 
-     #Notifies the user that a the openssl command has evaluated with the key $LINE$i
      then echo "Updated." 
     fi
-    echo "Pushing changes to GitHub with message 'Fixes and Updates'"
+    echo "Adding changes to GitHub with message 'Fixes and Updates'...."
     eval "git add ."
     eval "git commit -m 'Fixes and Updates'"
+    echo "Pushing...."
     eval "git push" 
     if [ $? -eq 0 ] 
-     #Notifies the user that a the openssl command has evaluated with the key $LINE$i
      then echo "Done." 
     fi
     exit 1
