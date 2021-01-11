2ef52e887f4cc586c15c125684006191f35aaddf
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-11 15:42:53 +0000

    Fixes and Updates

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	heroku "github.com/jonahgeorge/force-ssl-heroku"
@@ -52,32 +51,6 @@ func main() {
 	})
 	router.GET("/scan", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "scan.html", gin.H{"navtitle": "Scan."})
-	})
-
-	router.GET("/tick", func(c *gin.Context) {
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		rows, err := db.Query("SELECT tick FROM ticks")
-		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
-			return
-		}
-
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-		}
 	})
 
 	router.Run(":" + port)
