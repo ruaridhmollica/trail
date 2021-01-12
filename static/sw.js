e5b66d3325348ca1b0e30eab188fc00ae83fe49f
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-12 11:46:27 +0000

    Fixes and Updates

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,19 @@
 const cacheName = 'trail-v1';
 const staticAssets = [
-    '/main.go',
-   '/templates/',
+    './main.go',
+   './templates/',
    '/static/',
-   '/bin/'
+   './bin/',
+   '/templates/index.html',
+   '/templates/footer.html',
+   '/templates/head.html',
+   '/templates/map.html',
+   '/templates/mobnav.html',
+   '/templates/nav.html',
+   '/templates/scan.html',
+   '/templates/scripts.html',
+   '/templates/settings.html',
+   '/templates/tour.html'
 ];
 
 self.addEventListener('install', async e => {
