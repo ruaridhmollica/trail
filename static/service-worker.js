74eff87e0e1a9869e9b3bfb6b5cd7e0412104608
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-13 16:02:32 +0000

    Fixes and Updates

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,41 +1,7 @@
-self.addEventListener( "install", function( event ){
-    event.waitUntil(
-        caches.open( "static-cache" )
-              .then(function( cache ){
-            return cache.addAll([
-          
-            ]);
-        })
-    );
-    console.log('service worker installed');
+self.addEventListener('install', function(event) {
+    console.log("Service Worker Installed :)");
 });
 
-self.addEventListener( "fetch", event => {
-    const request = event.request,
-                    url = request.url;
-    // If we are requesting an HTML page.
-    if ( request.headers.get("Accept").includes("text/html") ) {
-        event.respondWith(
-            // Check the cache first to see if the asset exists, and if it does, return the cached asset.
-            caches.match( request ).then( cached_result => {
-                if ( cached_result ) {
-                    return cached_result;
-                }
-                // If the asset is not in the cache, fallback to a network request for the asset, and proceed to cache the result.
-                return fetch( request ).then( response => {
-                    const copy = response.clone();
-                    // Wait until the response we received is added to the cache.
-                    event.waitUntil(
-                        caches.open( "pages" )
-                              .then( cache => {
-                            return cache.put( request, response );
-                        })
-                    );
-                    return response;
-                })
-                // If the network is unavailable to make a request, pull the offline page out of the cache.
-                .catch(() => caches.match( "/templates/settings.html" ));
-            })
-        ); // end respondWith
-    } // end if HTML
-});
+self.addEventListener('activate', function(event) {
+    console.log("Service Worker Activated :)");
+  });
