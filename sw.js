35f0d15bdf0b4d87071ed236eb554f47ae1d4cba
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-13 18:53:35 +0000

    Fixes and Updates

--- sw.js
+++ sw.js
@@ -22,6 +22,28 @@ self.addEventListener('fetch', event => {
     );
 });
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e)=> {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted'){
+            console.log('User accepted A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', (evt) =>{
+    app.logEvent('a2hs', 'installed');
+});
+
   const CACHE_NAME = 'Trail-cache-v1';
   const cacheurls = [
       '/static/Trees.geojson',
