90df45fc0901602da5606ec4aac9b0d40e2e63c3
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-13 19:30:02 +0000

    Fixes and Updates

--- static/js/add.js
+++ static/js/add.js
@@ -1,20 +1,29 @@
 let deferredPrompt;
 
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
 window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
+    // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    btnAdd.style.display = 'block';
-    btnAdd.addEventListener('click', (e)=> {
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted'){
-                console.log('User accepted A2HS prompt');
-            }
-            deferredPrompt = null;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
         });
     });
-});
-
-window.addEventListener('appinstalled', (evt) =>{
-    app.logEvent('a2hs', 'installed');
-});
+  });
