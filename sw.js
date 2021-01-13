d642225bcda9366d9765b46b9c6235b38374f61e
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-13 18:55:55 +0000

    Fixes and Updates

--- sw.js
+++ sw.js
@@ -28,17 +28,18 @@ window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     deferredPrompt = e;
     btnAdd.style.display = 'block';
-});
-
-btnAdd.addEventListener('click', (e)=> {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted'){
-            console.log('User accepted A2HS prompt');
-        }
-        deferredPrompt = null;
+    btnAdd.addEventListener('click', (e)=> {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted'){
+                console.log('User accepted A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
     });
 });
+
+
 
 window.addEventListener('appinstalled', (evt) =>{
     app.logEvent('a2hs', 'installed');
