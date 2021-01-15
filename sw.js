2d215ee7f6f897a5718fc62382566bf50334156d
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-01-15 16:49:14 +0000

    Fixes and Updates

--- sw.js
+++ sw.js
@@ -54,6 +54,7 @@ self.addEventListener('fetch', event => {
       '/static/js/jquery.js',
       'manifest.webmanifest',
       'sw.js',
+      'main.go',
       '/static/facts.json',
       '/static/templates/footer.html',
       '/static/templates/head.html',
