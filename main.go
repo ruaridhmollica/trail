51c59c45bef0e6a0c50aaa8f6b74ba37a20c039f
Ruaridh Mollica <ruaridhmollica@gmail.com>
2021-02-03 15:39:12 +0000

    Fixes and Updates

--- main.go
+++ main.go
@@ -58,8 +58,8 @@ func main() {
 		}
 		//The following section of code handles the event in which a user scans a QR code of a specific tree (variable is passed in ? url param)
 		//temp := c.Request.URL.Query()
-		treeNum := c.GetQuery("id")
-		log("Tree ID is ?", treeNum)
+		treeNum := c.Query("id")
+		fmt.Println("Tree ID is ?", treeNum)
 		//treeNum := temp.Get("id")
 		if treeNum != "" {
 			rows, err := db.Query("SELECT treename, latinname, height, age, description, origin, img FROM trees WHERE id=?", treeNum)
